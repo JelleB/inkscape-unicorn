1a14a2dd3c9e397c7ea2e65feb7f1e68b16868f2
Jelle <jelle@ubuntu.ubuntu-domain>
2013-04-22 11:09:43 +0200

    initial commit halfway my changes TODO: make the trackwidth dependent on stoke width not some default TODO: assign different colors to different extruders, so you can print colours and different materials.

--- src/unicorn/context.py
+++ src/unicorn/context.py
@@ -2,122 +2,74 @@ from math import *
 import sys
 
 class GCodeContext:
-    def __init__(self, xy_feedrate, z_feedrate, start_delay, stop_delay, pen_up_angle, pen_down_angle, z_height, finished_height, x_home, y_home, register_pen, num_pages, continuous, file):
+    def __init__(self, xy_feedrate, z_feedrate, z_height, thread_width, temp, home, startGcode, file):
       self.xy_feedrate = xy_feedrate
       self.z_feedrate = z_feedrate
-      self.start_delay = start_delay
-      self.stop_delay = stop_delay
-      self.pen_up_angle = pen_up_angle
-      self.pen_down_angle = pen_down_angle
-      self.z_height = z_height
-      self.finished_height = finished_height
-      self.x_home = x_home
-      self.y_home = y_home
-      self.register_pen = register_pen
-      self.num_pages = num_pages
-      self.continuous = continuous
-      self.file = file
-      
+      self.zHeight = z_height
+      self.threadWidth = thread_width
+      self.temp = temp
+      self.home = home
+      self.startGcode = startGcode
+      self.file = file    
       self.drawing = False
+      self.retracted = False
       self.last = None
+      self.e = 0
 
       self.preamble = [
-        "(Scribbled version of %s @ %.2f)" % (self.file, self.xy_feedrate),
-        "( %s )" % " ".join(sys.argv),
+        "(3D printer gcode generated from %s )" % (self.file),
+        "( %s )" % " ".join(sys.argv).replace('\n','/'),
         "G21 (metric ftw)",
         "G90 (absolute mode)",
-        "G92 X%.2f Y%.2f Z%.2f (you are here)" % (self.x_home, self.y_home, self.z_height),
         ""
       ]
+      if startGcode != "" :
+        self.preamble.append(startGcode + "\n")   
+      
+      if home == "true" :
+        self.preamble.append("G28 X0 Y0 Z0 ; move to the endstops and call that position 0,0,0 \n")
 
       self.postscript = [
         "",
-				"(end of print job)",
-				"M300 S%0.2F (pen up)" % self.pen_up_angle,
-				"G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay),
-				"M300 S255 (turn off servo)",
-				"G1 X0 Y0 F%0.2F" % self.xy_feedrate,
-				"G1 Z%0.2F F%0.2F (go up to finished level)" % (self.finished_height, self.z_feedrate),
-				"G1 X%0.2F Y%0.2F F%0.2F (go home)" % (self.x_home, self.y_home, self.xy_feedrate),
-				"M18 (drives off)",
+        "(end of print job)",
+        "M104 S0",
+        "G91",
+        "G1 X10 Y10 Z5 E-5",
+        "G90",
+        "G28 X0 Y0" ,
+        "G84 (drives off)",
+        ""
       ]
 
       self.registration = [
-        "M300 S%d (pen down)" % (self.pen_down_angle),
-        "G4 P%d (wait %dms)" % (self.start_delay, self.start_delay),
-        "M300 S%d (pen up)" % (self.pen_up_angle),
-        "G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay),
-        "M18 (disengage drives)",
-        "M01 (Was registration test successful?)",
-        "M17 (engage drives if YES, and continue)",
-        ""
+         ""
       ]
 
       self.sheet_header = [
         "(start of sheet header)",
-        "G92 X%.2f Y%.2f Z%.2f (you are here)" % (self.x_home, self.y_home, self.z_height),
+        ""
       ]
-      if self.register_pen == 'true':
-        self.sheet_header.extend(self.registration)
-      self.sheet_header.append("(end of sheet header)")
 
       self.sheet_footer = [
-        "(Start of sheet footer.)",
-        "M300 S%d (pen up)" % (self.pen_up_angle),
-        "G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay),
-        "G91 (relative mode)",
-        "G0 Z15 F%0.2f" % (self.z_feedrate),
-        "G90 (absolute mode)",
-        "G0 X%0.2f Y%0.2f F%0.2f" % (self.x_home, self.y_home, self.xy_feedrate),
-        "M01 (Have you retrieved the print?)",
-        "(machine halts until 'okay')",
-        "G4 P%d (wait %dms)" % (self.start_delay, self.start_delay),
-        "G91 (relative mode)",
-        "G0 Z-15 F%0.2f (return to start position of current sheet)" % (self.z_feedrate),
-        "G0 Z-0.01 F%0.2f (move down one sheet)" % (self.z_feedrate),
-        "G90 (absolute mode)",
-        "M18 (disengage drives)",
-        "(End of sheet footer)",
-      ]
+        ""
+        ]
 
       self.loop_forever = [ "M30 (Plot again?)" ]
 
       self.codes = []
 
-    def generate(self):
-      if self.continuous == 'true':
-        self.num_pages = 1
-
-      codesets = [self.preamble]
-      if (self.continuous == 'true' or self.num_pages > 1):
-        codesets.append(self.sheet_header)
-      elif self.register_pen == 'true':
-        codesets.append(self.registration)
-      codesets.append(self.codes)
-      if (self.continuous == 'true' or self.num_pages > 1):
-        codesets.append(self.sheet_footer)
-
-      if self.continuous == 'true':
-        codesets.append(self.loop_forever)
-        for codeset in codesets:
-          for line in codeset:
-            print line
-      else:
-        for p in range(0,self.num_pages):
-          for codeset in codesets:
-            for line in codeset:
-              print line
-          for line in self.postscript:
-            print line
-
     def start(self):
-      self.codes.append("M300 S%0.2F (pen down)" % self.pen_down_angle)
-      self.codes.append("G4 P%d (wait %dms)" % (self.start_delay, self.start_delay))
+      if (self.retracted):
+        self.e += 5
+        self.codes.append("G1 E%0.4f (de-retract some)" %(self.e))
+        self.retracted = False
       self.drawing = True
 
     def stop(self):
-      self.codes.append("M300 S%0.2F (pen up)" % self.pen_up_angle)
-      self.codes.append("G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay))
+      if (self.retracted == False):
+        self.e += -5
+        self.codes.append("G1 E%0.4f (retract some)" %(self.e))
+        self.retracted = True
       self.drawing = False
 
     def go_to_point(self, x, y, stop=False):
@@ -127,21 +79,40 @@ class GCodeContext:
         return
       else:
         if self.drawing: 
-            self.codes.append("M300 S%0.2F (pen up)" % self.pen_up_angle) 
-            self.codes.append("G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay))
-            self.drawing = False
-        self.codes.append("G1 X%.2f Y%.2f F%.2f" % (x,y, self.xy_feedrate))
+          self.stop(self)
+        self.codes.append("G0 X%.2f Y%.2f F%.2f" % (x,y, self.xy_feedrate * 60))
       self.last = (x,y)
 	
-    def draw_to_point(self, x, y, stop=False):
+    def draw_to_point(self, x, y, width, layerHeight, stop=False):
       if self.last == (x,y):
           return
       if stop:
         return
       else:
         if self.drawing == False:
-            self.codes.append("M300 S%0.2F (pen down)" % self.pen_up_angle)
-            self.codes.append("G4 P%d (wait %dms)" % (self.start_delay, self.start_delay))
-            self.drawing = True
-        self.codes.append("G1 X%0.2f Y%0.2f F%0.2f" % (x,y, self.xy_feedrate))
+          self.start(self)
+        lx = x - self.last[0]
+        ly = y - self.last[1]  
+        self.e += self.calculateE( lx , ly , self.zHeight, self.threadWidth)  
+        self.codes.append("G1 X%0.2f Y%0.2f E%0.3f F%0.2f" % (x,y,self.e, self.xy_feedrate * 60))
       self.last = (x,y)
+      
+    def calculateE(self, x, y, z, thread, radius=1.45):
+      #"calculate the relative E component based on x y z vector and thread width"
+      l = hypot(x,y)
+      return (l * z * thread ) / (pi * radius * radius)
+      
+      
+    def generate(self):
+    
+      codesets = [self.codes]
+      
+      for line in self.preamble:
+        print line
+
+      for codeset in codesets:
+        for line in codeset:
+            print line
+        if codeset :    
+          for line in self.postscript:
+              print line
